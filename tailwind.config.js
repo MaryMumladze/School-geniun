1a053b58e90f0784cfeb3df207e975652bfb87e3
MaryMumladze <mmumladzemari12@gmail.com>
2024-02-16 17:12:57 +0400

    add fixed header

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,15 @@ module.exports = {
         'blue': "#184C99",
         'yellow': "#FFC652",
         'bold-red': "#E74040",
+        'danger-color': "#E74040",
         'dark-blue': "#26335D",
+        'black-text': "#252B42",
+        'second-text': "#737373",
       }
     },
+    fontFamily: {
+      monst: ['Montserrat'],
+    }
   },
   plugins: [],
 }
